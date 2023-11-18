fe83b7c941eb58becd022b01693c5a141c7babd6
Kyle Upton <kyleaupton@gmail.com>
2023-11-18 17:35:09 -0500

    way better dev mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,18 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { Document, Page, pdfjs } from 'react-pdf'
 import { usePDF } from '@react-pdf/renderer'
 import PDF from './pdf'
-import 'react-pdf/dist/Page/AnnotationLayer.css'
-import 'react-pdf/dist/Page/TextLayer.css'
 import './main.css'
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.js',
-  import.meta.url
-).toString()
 
 const App = () => {
   const [instance] = usePDF({ document: PDF })
 
   if (instance.loading) return <div>Loading...</div>
-  if (instance.error) return <div>Something went wrong: {instance.error}</div>
+  if (instance.error || !instance.url) return <div>Something went wrong: {instance.error}</div>
 
   return (
     <div className='pdf-wrapper'>
-      <Document key={instance.url} file={instance.url}>
-        <Page pageNumber={1} scale={1} />
-      </Document>
+      <iframe src={instance.url} />
     </div>
   )
 }
