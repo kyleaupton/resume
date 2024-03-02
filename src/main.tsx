d9260e57c2675a08c32257ce6ad29a2751a31e27
Kyle Upton <kyleaupton@gmail.com>
2024-03-02 11:51:09 -0500

    dynamic data wip

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import { usePDF } from '@react-pdf/renderer'
+import { Octokit } from 'octokit'
 import PDF from './pdf'
+import { projects } from './projects'
 import './main.css'
 
+const octokit = new Octokit();
+
+let cache: any[] | null = null
+
 const App = () => {
+  const [data, setData] = useState(projects)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const getProjectData = async () => {
+      if (cache) {
+        setData(cache)
+        setLoading(false)
+        return
+      }
+
+      const payload = []
+
+      try {
+        for (const project of projects) {
+          console.log('making api request')
+          payload.push((await octokit.request(`GET /repos/${project.id}`)).data);
+        }
+        setData(payload)
+        cache = payload
+      } catch (error) {
+        console.error(error)
+        setError(error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    getProjectData()
+  }, [])
+
   const [instance] = usePDF({ document: PDF })
 
-  if (instance.loading) return <div>Loading...</div>
-  if (instance.error || !instance.url) return <div>Something went wrong: {instance.error}</div>
+  if (instance.loading || loading) return <div>Loading...</div>
+  if (instance.error || !instance.url || error) return <div>Something went wrong: {instance.error}</div>
 
   return (
     <div className='pdf-wrapper'>
@@ -17,8 +55,6 @@ const App = () => {
   )
 }
 
-createRoot(document.getElementById('root')!).render( // eslint-disable-line
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <App />
 )
---
+++ src/projects.ts
@@ -0,0 +1,42 @@
+export const projects = [
+  {
+    id: 'kyleaupton/boot-builder',
+    icons: ['vue', 'electron', 'typescript'],
+  },
+  {
+    id: 'kyleaupton/resume',
+    icons: ['react', 'typescript'],
+  },
+  {
+    id: 'kyleaupton/node-rsync',
+    npm: 'https://www.npmjs.com/package/@kyleupton/node-rsync',
+  },
+  {
+    id: 'kyleaupton/glob-copy',
+    npm: 'https://www.npmjs.com/package/@kyleupton/glob-copy',
+  },
+  {
+    id: 'kyleaupton/portfolio',
+    icons: ['vue', 'typescript'],
+  },
+  {
+    id: 'kyleaupton/TransactionTracker',
+  },
+  {
+    id: 'kyleaupton/schedule-widget',
+  },
+  {
+    id: 'kyleaupton/zerotier-utility',
+    icons: ['vue', 'electron', 'javascript'],
+  },
+  {
+    id: 'kyleaupton/witte-quote-generator-desktop',
+    icons: ['vue', 'electron', 'javascript'],
+  },
+  {
+    id: 'kyleaupton/win-iso',
+  },
+  {
+    id: 'kyleaupton/leetcode-reminder',
+  }, 
+]
