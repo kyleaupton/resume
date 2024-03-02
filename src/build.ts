ae758ba3829f43b122eb1aea2ff36f788ca0e678
Kyle Upton <kyleaupton@gmail.com>
2024-03-02 14:11:55 -0500

    node version bump

--- src/build.ts
+++ src/build.ts
@@ -1,12 +1,12 @@
 import fs from 'fs/promises';
 import path from 'path';
-// import url from 'url';
+import url from 'url';
 import { renderToFile } from '@react-pdf/renderer';
 import PDF from './pdf';
 
 const doRender = async () => {
-  // const dist = path.resolve(path.dirname(url.fileURLToPath(import.meta.url)), '..', 'dist');
-  const dist = path.resolve(__dirname, '..', 'dist');
+  const dist = path.resolve(path.dirname(url.fileURLToPath(import.meta.url)), '..', 'dist');
+  // const dist = path.resolve(__dirname, '..', 'dist');
   await fs.mkdir(dist, { recursive: true });
   await renderToFile(PDF, path.join(dist, 'out.pdf'));
 };
